03e9d4b25cc2837123c593c78683ca8ad3a8d160
HuskyRye <1412167533@qq.com>
2022-07-24 17:07:55 +0000

    Added a test file.

---
+++ personal/proj-pregame/src/tests/userprog/compute-e.c
@@ -0,0 +1,18 @@
+/* Child process run by floating point tests
+   Uses Taylor definition to compute the Euler constant e */
+
+#include <float.h>
+#include "tests/lib.h"
+
+const char* test_name = "compute-e";
+
+int main(void) {
+  double e_res = sum_to_e(10);
+  if (abs_val(e_res - E_VAL) < TOL) {
+    msg("Success!");
+    exit(162);
+  } else {
+    msg("Got e=%f, expected e=%f", e_res, E_VAL);
+    exit(126);
+  }
+}
---
+++ personal/proj-pregame/src/tests/userprog/fp-kernel-e.c
@@ -0,0 +1,20 @@
+/* Verifies that the compute_e system call is implemented correctly */
+
+#include <float.h>
+#include <syscall.h>
+#include "tests/lib.h"
+#include "tests/main.h"
+
+const char* test_name = "fp-kernel-e";
+
+void test_main(void) {
+  msg("Computing e...");
+  double e_res = compute_e(10);
+  if (abs_val(e_res - E_VAL) < TOL) {
+    msg("Success!");
+    exit(162);
+  } else {
+    msg("Got e=%f, expected e=%f", e_res, E_VAL);
+    exit(126);
+  }
+}
