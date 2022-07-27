3eeff4424cdd8f29ff4df0ac74b700d4f1aa26ac
HuskyRye <1412167533@qq.com>
2022-07-27 06:57:12 +0000

    Finish limit, gdb and gcc

--- personal/hw-intro/limits.c
+++ personal/hw-intro/limits.c
@@ -2,9 +2,12 @@
 #include <sys/resource.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
-    return 0;
+  struct rlimit lim;
+  getrlimit(RLIMIT_STACK, &lim);
+  printf("stack size: %ld\n", lim.rlim_cur);
+  getrlimit(RLIMIT_NPROC, &lim);
+  printf("process limit: %ld\n", lim.rlim_cur);
+  getrlimit(RLIMIT_NOFILE, &lim);
+  printf("max file descriptors: %ld\n", lim.rlim_cur);
+  return 0;
 }
