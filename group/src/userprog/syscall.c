f00c7e9881bc88801880a60d91e73b89e7ce2b75
HuskyRye <1412167533@qq.com>
2022-08-08 10:49:11 +0000

    Handled invalid arguments.

--- group/src/userprog/syscall.c
+++ group/src/userprog/syscall.c
@@ -4,36 +4,65 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "userprog/process.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
+static void exit(int status);
+static void verify_arg_vaddr(uint8_t* vaddr);
+static void verify_string(char* str);
+
 static void syscall_handler(struct intr_frame* f UNUSED) {
   uint32_t* args = ((uint32_t*)f->esp);
+  verify_arg_vaddr(args);
 
-  /*
-   * The following print statement, if uncommented, will print out the syscall
-   * number whenever a process enters a system call. You might find it useful
-   * when debugging. It will cause tests to fail, however, so you should not
-   * include it in your final submission.
-   */
+  switch (args[0]) {
+    case SYS_READ:
+    case SYS_WRITE:
+      verify_arg_vaddr(&args[3]);
+    case SYS_CREATE:
+    case SYS_SEEK:
+      verify_arg_vaddr(&args[2]);
+    case SYS_EXIT:
+    case SYS_EXEC:
+    case SYS_WAIT:
+    case SYS_REMOVE:
+    case SYS_OPEN:
+    case SYS_FILESIZE:
+    case SYS_TELL:
+    case SYS_CLOSE:
+    case SYS_PRACTICE:
+      verify_arg_vaddr(&args[1]);
+    case SYS_HALT:
+    default:
+      break;
+  }
 
-  /* printf("System call number: %d\n", args[0]); */
   switch (args[0]) {
     case SYS_HALT:
       shutdown_power_off();
       break;
     case SYS_EXIT:
-      f->eax = args[1];
-      thread_current()->pcb->exit_status = args[1];
-      process_exit();
+      exit(args[1]);
       break;
     case SYS_EXEC:
+      verify_string(args[1]);
       f->eax = process_execute(args[1]);
       break;
     case SYS_WAIT:
       f->eax = process_wait(args[1]);
+      break;
+    case SYS_CREATE:
+      verify_string(args[1]);
+      break;
+    case SYS_REMOVE:
+      verify_string(args[1]);
+      break;
+    case SYS_OPEN:
+      verify_string(args[1]);
       break;
     case SYS_WRITE:
       if (args[1] == STDOUT_FILENO) {
@@ -46,5 +75,31 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
       break;
     default:
       break;
+  }
+}
+
+static void exit(int status) {
+  thread_current()->pcb->exit_status = status;
+  process_exit();
+}
+
+static inline bool valid_vaddr(uint8_t* vaddr) {
+  uint32_t* pagedir = thread_current()->pcb->pagedir;
+  return vaddr != NULL && is_user_vaddr(vaddr) && pagedir_get_page(pagedir, vaddr) != NULL;
+}
+
+static void verify_arg_vaddr(uint8_t* vaddr) {
+  if (!valid_vaddr(vaddr) || !valid_vaddr(vaddr + 1) || !valid_vaddr(vaddr + 2) ||
+      !valid_vaddr(vaddr + 3))
+    exit(-1);
+}
+
+static void verify_string(char* str) {
+  while (true) {
+    if (!valid_vaddr(str))
+      exit(-1);
+    if (*str == '\0')
+      return;
+    ++str;
   }
 }
