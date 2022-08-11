90f0e91a420a8d45b4e63a8f10e04c1196b06151
HuskyRye <1412167533@qq.com>
2022-08-11 09:14:31 +0000

    Finished series pipes.

--- personal/hw-shell/shell.c
+++ personal/hw-shell/shell.c
@@ -17,6 +17,7 @@
 
 /* Convenience macro to silence compiler warnings about unused function parameters. */
 #define unused __attribute__((unused))
+#define NO_RETURN __attribute__((noreturn))
 
 /* Whether the shell is connected to an actual terminal or not. */
 bool shell_is_interactive;
@@ -151,6 +152,157 @@ void init_shell() {
   }
 }
 
+void child_process(int start, int end, struct tokens* tokens) {
+  char** argv = malloc(sizeof(char*) * (end - start + 1));
+  int k = 0;
+  for (int i = start; i < end; ++i) {
+    char* token = tokens_get_token(tokens, i);
+    if (token[0] == '>') {
+      token = tokens_get_token(tokens, ++i);
+      int fd = open(token, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+      dup2(fd, STDOUT_FILENO);
+    } else if (token[0] == '<') {
+      token = tokens_get_token(tokens, ++i);
+      int fd = open(token, O_RDONLY);
+      if (fd == -1) {
+        perror(token);
+        free(argv);
+        tokens_destroy(tokens);
+        exit(-1);
+      }
+      dup2(fd, STDIN_FILENO);
+    } else {
+      argv[k++] = token;
+    }
+  }
+  argv[k] = NULL;
+  const char* exe = tokens_get_token(tokens, start);
+  size_t exe_len = strlen(exe);
+  char* cmd;
+  if (exe[0] == '~') { /* Replace ~ with absolute path */
+    uid_t uid = getuid();
+    struct passwd* pwd = getpwuid(uid);
+    if (pwd == NULL) {
+      printf("User with %u ID is unknown.\n", uid);
+      free(argv);
+      tokens_destroy(tokens);
+      exit(-1);
+    }
+    const char* home = pwd->pw_dir;
+    size_t home_len = strlen(home);
+    cmd = malloc(home_len + exe_len);
+    strcpy(cmd, home);
+    strcpy(cmd + home_len, exe + 1);
+  } else {
+    cmd = malloc(exe_len + 1);
+    strcpy(cmd, exe);
+  }
+
+  if (strchr(cmd, '/')) { /* File name can NOT contain '/', so cmd can't be program in PATH */
+    if (execv(cmd, argv) == -1) {
+      if (errno == 13) { // Permission denied
+        struct stat stat_buf;
+        stat(cmd, &stat_buf);
+        if (S_ISDIR(stat_buf.st_mode)) { // A directory
+          printf("%s: Is a directory\n", cmd);
+        } else { // A file
+          perror(cmd);
+        }
+      } else if (exe[0] == '~') {
+        printf("%s: No such file or directory\n", exe);
+      } else {
+        perror(cmd);
+      }
+    }
+  } else { /* Program in PATH */
+    char* env_path = getenv("PATH");
+    size_t cmd_len = strlen(cmd);
+    char* temp = strtok(env_path, ":");
+    while (temp != NULL) {
+      size_t temp_len = strlen(temp);
+      char* path = malloc(temp_len + 1 + cmd_len + 1);
+      strcpy(path, temp);
+      path[temp_len] = '/';
+      strcpy(path + temp_len + 1, cmd);
+      if (access(path, X_OK) == 0) {
+        execv(path, argv);
+      }
+      free(path);
+      temp = strtok(NULL, ":");
+    }
+    printf("%s: command not found\n", cmd);
+  }
+  /* Reach here means execv failed */
+  free(cmd);
+  free(argv);
+  tokens_destroy(tokens);
+  exit(-1);
+}
+
+int execute(int read_fd, int* pipe_fd, int start, int end, struct tokens* tokens) {
+  pid_t pid = fork();
+  if (pid == -1) {
+    perror("fork");
+    return -1;
+  } else if (pid == 0) { /* Child process */
+    if (read_fd != -1) {
+      dup2(read_fd, STDIN_FILENO); // Read from previous pipe's read end
+    }
+    if (pipe_fd != NULL) {
+      dup2(pipe_fd[1], STDOUT_FILENO); // Write to current pipe's write end
+    }
+    child_process(start, end, tokens); // NORETURN
+  } else {                             /* Parent process */
+    int status;
+    wait(&status);
+  }
+  return 0;
+}
+
+int run_programs(struct tokens* tokens) {
+  size_t tokens_len = tokens_get_length(tokens);
+  int read_fd = -1;          // Previous pipe's read end
+  int pipe_fd[2] = {-1, -1}; // Next read, current write
+  int start = 0;
+  for (int i = 0; i < tokens_len; ++i) {
+    char* token = tokens_get_token(tokens, i);
+    if (token[0] == '|') {
+      if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        if (read_fd != -1) {
+          close(read_fd);
+        }
+        return -1;
+      }
+      if (execute(read_fd, pipe_fd, start, i, tokens) == -1) {
+        if (read_fd != -1) {
+          close(read_fd);
+        }
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        return -1;
+      }
+      if (read_fd != -1) {
+        close(read_fd);
+      }
+      close(pipe_fd[1]);
+      read_fd = pipe_fd[0];
+      start = i + 1;
+    }
+  }
+  /* Last or the only one */
+  if (execute(read_fd, NULL, start, tokens_len, tokens) == -1) {
+    if (read_fd != -1) {
+      close(read_fd);
+    }
+    return -1;
+  }
+  if (read_fd != -1) {
+    close(read_fd);
+  }
+  return 0;
+}
+
 int main(unused int argc, unused char* argv[]) {
   init_shell();
 
@@ -171,99 +323,7 @@ int main(unused int argc, unused char* argv[]) {
     if (fundex >= 0) {
       cmd_table[fundex].fun(tokens);
     } else {
-      pid_t pid = fork();
-      if (pid < 0) {
-        perror("fork");
-      } else if (pid == 0) { // new process
-        size_t tokens_len = tokens_get_length(tokens);
-        char** argv = malloc(sizeof(char*) * (tokens_len + 1));
-        char** p = argv;
-        for (int i = 0; i < tokens_len; ++i) {
-          char* token = tokens_get_token(tokens, i);
-          if (token[0] == '>') {
-            ++i;
-            token = tokens_get_token(tokens, i);
-            int fd = open(token, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-            dup2(fd, STDOUT_FILENO);
-          } else if (token[0] == '<') {
-            ++i;
-            token = tokens_get_token(tokens, i);
-            int fd = open(token, O_RDONLY);
-            if (fd == -1) {
-              perror(token);
-              free(argv);
-              tokens_destroy(tokens);
-              return -1;
-            }
-            dup2(fd, STDIN_FILENO);
-          } else {
-            *p = token;
-            ++p;
-          }
-        }
-        *p = NULL;
-        const char* exe = tokens_get_token(tokens, 0);
-        size_t exe_len = strlen(exe);
-        char* cmd;
-        if (exe[0] == '~') { /* Replace ~ with absolute path */
-          uid_t uid = getuid();
-          struct passwd* pwd = getpwuid(uid);
-          if (!pwd) {
-            printf("User with %u ID is unknown.\n", uid);
-            return -1;
-          }
-          const char* home = pwd->pw_dir;
-          size_t home_len = strlen(home);
-          cmd = malloc(home_len + exe_len);
-          strcpy(cmd, home);
-          strcpy(cmd + home_len, exe + 1);
-        } else {
-          cmd = malloc(exe_len + 1);
-          strcpy(cmd, exe);
-        }
-
-        if (strchr(cmd, '/')) { // File name can NOT contain '/', so cmd can't be program in PATH
-          if (execv(cmd, argv) == -1) {
-            if (errno == 13) { // Permission denied
-              struct stat stat_buf;
-              stat(cmd, &stat_buf);
-              if (S_ISDIR(stat_buf.st_mode)) { // A directory
-                printf("%s: Is a directory\n", cmd);
-              } else { // A file
-                perror(cmd);
-              }
-            } else if (exe[0] == '~') {
-              printf("%s: No such file or directory\n", exe);
-            } else {
-              perror(cmd);
-            }
-          }
-        } else {
-          char* env_path = getenv("PATH");
-          size_t cmd_len = strlen(cmd);
-          char* temp = strtok(env_path, ":");
-          while (temp != NULL) {
-            size_t temp_len = strlen(temp);
-            char* path = malloc(temp_len + 1 + cmd_len + 1);
-            strcpy(path, temp);
-            path[temp_len] = '/';
-            strcpy(path + temp_len + 1, cmd);
-            if (access(path, X_OK) == 0) {
-              execv(path, argv);
-            }
-            free(path);
-            temp = strtok(NULL, ":");
-          }
-          printf("%s: command not found\n", cmd);
-        }
-        free(cmd);
-        free(argv);
-        tokens_destroy(tokens);
-        return -1;
-      } else {
-        int status;
-        wait(&status);
-      }
+      run_programs(tokens);
     }
 
     if (shell_is_interactive)
